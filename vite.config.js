e729a1c48b11ee7e4bd86888c93f2b53e41e5811
Aliaskar <aliaskar2014@mail.ru>
2026-07-16 18:06:08 +0500

    vite obnova

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss(), // Возвращаем Tailwind на место
   ],
+  base: '/', // <-- ВНИМАНИЕ: Здесь нужно оставить ТОЛЬКО название репозитория!
 })
